9af28e6b2b13719e26e946e028d7d4b9fa73d667
nassmim <nassmim972@gmail.com>
2023-06-13 02:37:11 +0100

    fix: employees table columns dynamically created

--- src/features/ViewEmployees/Table/TableHead.tsx
+++ src/features/ViewEmployees/Table/TableHead.tsx
@@ -6,165 +6,33 @@ const TableHead = ({ columns }: { columns: TableColumn[] }) => {
   return (
     <thead>
       <tr>
-        <th>
-          <div className="title">
-            <p>First Name</p>
-            <div className="arrows">
-              <Arrow
-                src={PaginateLeftArrow}
-                alt="Sort descending"
-                width="10px"
-                rotate="90deg"
-                className="sort-arrow"
-                cursor="pointer"
-              />
-              <Arrow
-                src={PaginateLeftArrow}
-                alt="Sort ascending"
-                width="10px"
-                rotate="-90deg"
-                className="sort-arrow"
-                cursor="pointer"
-              />
-            </div>
-          </div>
-        </th>
-        <th>
-          Last Name
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          Start Date
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          Department
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          Date of Birth
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          Street
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          City
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          State
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
-        <th>
-          Zipcode
-          <div className="arrows">
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort ascending"
-              width="10px"
-              rotate="-90deg"
-            />
-            <Arrow
-              src={PaginateLeftArrow}
-              alt="Sort descending"
-              width="10px"
-              rotate="90deg"
-            />
-          </div>
-        </th>
+        {columns.map(
+          ({ label, accessor }: { label: string; accessor: string }) => (
+            <th key={accessor}>
+              <div className="title">
+                <p>{label}</p>
+                <div className="arrows">
+                  <Arrow
+                    src={PaginateLeftArrow}
+                    alt="Sort descending"
+                    width="10px"
+                    rotate="90deg"
+                    className="sort-arrow"
+                    cursor="pointer"
+                  />
+                  <Arrow
+                    src={PaginateLeftArrow}
+                    alt="Sort ascending"
+                    width="10px"
+                    rotate="-90deg"
+                    className="sort-arrow"
+                    cursor="pointer"
+                  />
+                </div>
+              </div>
+            </th>
+          )
+        )}
       </tr>
     </thead>
   )
